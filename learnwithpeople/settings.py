7c3cfed1882e73ffdb4197e69e4286cbaf69ed62
Dirk Uys <dirkcuys@gmail.com>
2015-04-21 20:27:07 -0400

    Add default value for local db for testing using travisci

--- learnwithpeople/settings.py
+++ learnwithpeople/settings.py
@@ -121,7 +121,7 @@ DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 ##### Heroku config
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='sqlite:///{0}'.format(path('db.sqlite')))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
