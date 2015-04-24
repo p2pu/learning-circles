8b3132f9aec26d71498a153a29ea8d2049f07da6
Dirk Uys <dirkcuys@gmail.com>
2015-04-24 10:33:50 -0400

    Add applications to study groups

--- studygroups/admin.py
+++ studygroups/admin.py
@@ -3,20 +3,31 @@ from django.contrib import admin
 # Register your models here.
 from studygroups.models import Course, StudyGroup, StudyGroupSignup, Application
 
-class CourseAdmin(admin.ModelAdmin):
-    pass
-
 class StudyGroupSignupInline(admin.TabularInline):
     model = StudyGroupSignup
 
+class ApplicationInline(admin.TabularInline):
+    model = Application.study_groups.through
+    readonly_fields = ['user_name']
+    def user_name(self, instance):
+        return instance.application.name
+    user_name.short_description = 'user name'
+
 class StudyGroupAdmin(admin.ModelAdmin):
-    inlines = [StudyGroupSignupInline]
+    inlines = [
+        #StudyGroupSignupInline,
+        ApplicationInline,
+    ]
 
 class StudyGroupSignupAdmin(admin.ModelAdmin):
     pass
 
-class ApplicationAdmin(admin.ModelAdmin):
+class CourseAdmin(admin.ModelAdmin):
     pass
+
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_method', 'created_at')
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Application, ApplicationAdmin)
