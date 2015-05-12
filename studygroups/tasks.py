c15bc71a084513adb6714ff14280f2cb055c9053
Dirk Uys <dirkcuys@gmail.com>
2015-05-12 16:21:35 -0500

    Update tasks to use function defined in models to send notifications

--- studygroups/tasks.py
+++ studygroups/tasks.py
@@ -3,22 +3,15 @@ from django.utils import timezone
 from studygroups.models import StudyGroup
 from studygroups.models import Reminder
 from studygroups.models import generate_reminder
-from studygroups.models import send_group_message
+from studygroups.models import send_reminder
 
 import datetime
 
 def send_reminders():
     now = timezone.now()
     for reminder in Reminder.objects.filter(sent_at__isnull=True):
-        if reminder.meeting_date - now < datetime.timedelta(days=2):
-            send_group_message(
-                reminder.study_group,
-                reminder.email_subject,
-                reminder.email_body,
-                reminder.sms_body
-            )
-            reminder.sent_at = now
-            reminder.save()
+        if reminder.meeting_time - now < datetime.timedelta(days=2):
+            send_reminder(reminder)
 
 
 def gen_reminders():
