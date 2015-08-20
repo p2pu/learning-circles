56aa1f98f626d7e5c389c39ad94910b7e0bbcb21
Dirk Uys <dirkcuys@gmail.com>
2015-08-20 08:30:14 -0600

    Fix issue with tiling of LCs on homepage

--- static/js/main.js
+++ static/js/main.js
@@ -23,21 +23,6 @@ var P2PU = window.P2PU || {};
                     }
                 }
             });
-
-            function equalHeight(group) {    
-                var tallest = 0;    
-                group.each(function() {       
-                    var thisHeight = $(this).height();       
-                    if(thisHeight > tallest) {          
-                        tallest = thisHeight;       
-                    }    
-                });    
-                group.each(function() { $(this).height(tallest); });
-            } 
-
-            $(document).ready(function() {   
-                equalHeight($(".thumbnail").parent()); 
-            });
         });
     };
 
