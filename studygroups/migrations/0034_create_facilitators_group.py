5e412494e09d845dcb08529bd9c436f52cdda91b
Dirk Uys <dirkcuys@gmail.com>
2015-08-26 14:49:54 -0600

    Change data migration to work even if facilitator group already exists

--- studygroups/migrations/0034_create_facilitators_group.py
+++ studygroups/migrations/0034_create_facilitators_group.py
@@ -5,8 +5,7 @@ from django.db import models, migrations
 
 def create_facilitators_group(apps, schema_editor):
     Group = apps.get_model("auth", "Group")
-    group = Group(name="facilitators")
-    group.save()
+    Group.objects.get_or_create(name="facilitators")
 
 
 class Migration(migrations.Migration):
