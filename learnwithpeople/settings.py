17d96064aa836a9d07e8f80139e181b692b1e038
Dirk Uys <dirkcuys@gmail.com>
2015-10-01 15:25:11 -0600

    Email logged errors to system admin

--- learnwithpeople/settings.py
+++ learnwithpeople/settings.py
@@ -173,6 +173,28 @@ CELERYBEAT_SCHEDULE = {
     },
 }
 
+LOGGING = {
+    'version': 1,
+    'dissable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['mail_admins', 'console'],
+            'level': 'DEBUG',
+        }
+    },
+}
+
 
 ##### Support for settings_local.py
 try:
