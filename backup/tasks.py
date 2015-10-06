ddf3b0f09a67ae3f6865fd540e4b0c68cc1e32d9
Dirk Uys <dirkcuys@gmail.com>
2015-10-06 11:17:35 -0600

    Fix error with missing import and wrong name for error!

--- backup/tasks.py
+++ backup/tasks.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from celery import shared_task
-from backup.utils import backup_media, backup_db
+from backup.utils import backup_media, backup_db, BackupFailed
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,7 +10,7 @@ def make_backup():
     try:
         backup_media()
         backup_db()
-    except BackFailed as e:
+    except BackupFailed as e:
         logger.exception('Backup failed')
     else:
         logger.info('Backup created')
