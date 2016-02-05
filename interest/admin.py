53d33daaa0f1b811206aa0cb8b5f99910c640fd2
Dirk Uys <dirkcuys@gmail.com>
2016-02-05 14:43:15 +0200

    Add interest model to admin interface

--- interest/admin.py
+++ interest/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from interest.models import Lead
+
+
+class LeadAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Lead, LeadAdmin)
+
