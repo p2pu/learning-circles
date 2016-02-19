91c2d03b92d0c773a11ae0ca49e4035df6f80858
Dirk Uys <dirkcuys@gmail.com>
2016-02-19 14:41:16 +0200

    Don't send reminders for deleted meetings or learning circles

--- studygroups/tasks.py
+++ studygroups/tasks.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from django.conf import settings
 
 from studygroups.models import StudyGroup
+from studygroups.models import StudyGroupMeeting
 from studygroups.models import Reminder
 from studygroups.models import generate_reminder
 from studygroups.models import send_reminder
@@ -19,7 +20,8 @@ def send_reminders():
     now = timezone.now()
     translation.activate(settings.LANGUAGE_CODE)
     # TODO - should this be set here or closer to where the language matters?
-    for reminder in Reminder.objects.filter(sent_at__isnull=True):
+    # TODO - make sure both the StudyGroup and StudyGroupMeeting is still available
+    for reminder in Reminder.objects.filter(sent_at__isnull=True, study_group__in=StudyGroup.objects.active(), study_group_meeting__in=StudyGroupMeeting.objects.active()):
         if reminder.study_group_meeting and reminder.study_group_meeting.meeting_time - now < datetime.timedelta(days=2):
             send_reminder(reminder)
 
