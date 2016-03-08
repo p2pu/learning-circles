8e3311015607f8df591d05812b2febb882eb5e93
Dirk Uys <dirkcuys@gmail.com>
2016-03-08 10:36:43 +0200

    lowercase all city names when searching4

--- uxhelpers/views.py
+++ uxhelpers/views.py
@@ -6,6 +6,6 @@ from utils import json_response
 def city_complete(request):
     # response format 'City name, region code, country name'
     callback = request.GET.get('callback')
-    q = request.GET.get('q')
-    matches = [ city for city in c.read_autocomplete_list() if city.find(q) != -1 ]
+    q = request.GET.get('q').lower()
+    matches = [ city for city in c.read_autocomplete_list() if city.lower().find(q) != -1 ]
     return json_response(request, matches)
