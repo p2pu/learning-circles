a8aa90446b93865c8816eaaca74b38836dee5b5f
Dirk Uys <dirkcuys@gmail.com>
2016-03-08 12:29:58 +0200

    Make city field mandatory & display on Learning Circle cards

---
+++ studygroups/migrations/0062_auto_20160308_1026.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('studygroups', '0061_auto_20160224_1357'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='studygroup',
+            name='city',
+            field=models.CharField(max_length=256),
+        ),
+    ]
