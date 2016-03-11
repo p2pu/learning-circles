d75f10be9a86ea6f4417b5c8ac01ba92202c0907
Dirk Uys <dirkcuys@gmail.com>
2016-03-11 15:05:10 +0200

    Fix migrations for postgres

--- studygroups/migrations/0064_split63.py
+++ studygroups/migrations/0064_split63.py
@@ -5,25 +5,25 @@ from django.db import migrations, models
 import datetime
 from django.utils.timezone import utc
 
-def set_meeting_date(apps, schema_editor):
+
+def set_meeting_time(apps, schema_editor):
     StudyGroupMeeting = apps.get_model('studygroups', 'StudyGroupMeeting')
     for meeting in StudyGroupMeeting.objects.all():
-        meeting.meeting_date = meeting.meeting_time.date()
+        meeting.meeting_time = meeting.study_group.meeting_time
         meeting.save()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('studygroups', '0062_auto_20160308_1026'),
+        ('studygroups', '0063_auto_20160309_1301'),
     ]
 
     operations = [
-        migrations.AddField(
+        migrations.AlterField(
             model_name='studygroupmeeting',
-            name='meeting_date',
-            field=models.DateField(default=datetime.datetime(2016, 3, 9, 13, 1, 56, 898627, tzinfo=utc)),
-            preserve_default=False,
+            name='meeting_time',
+            field=models.TimeField(),
         ),
-        migrations.RunPython(set_meeting_date),
+        migrations.RunPython(set_meeting_time),
     ]
