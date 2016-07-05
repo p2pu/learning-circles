d85c9d6f7de251f9a71385ef564151875012967e
Dirk Uys <dirkcuys@gmail.com>
2016-07-05 15:15:39 +0200

    Remove interest from admin interface

--- interest/admin.py
+++ interest/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from interest.models import Lead
-
-
-class LeadAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Lead, LeadAdmin)
-
