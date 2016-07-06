8ac9cd4a0f3b68043a0ea2021304055a65d47936
Dirk Uys <dirkcuys@gmail.com>
2016-07-06 14:59:33 +0200

    Update admin for studygroups

--- studygroups/admin.py
+++ studygroups/admin.py
@@ -1,29 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
-from studygroups.models import Course, StudyGroup, StudyGroupMeeting, Application, Reminder, Activity
+from studygroups.models import Course
+from studygroups.models import StudyGroup 
+from studygroups.models import StudyGroupMeeting 
+from studygroups.models import Application 
+from studygroups.models import Reminder 
+from studygroups.models import Activity
 from studygroups.models import Organizer
 from studygroups.models import Facilitator
+from studygroups.models import Team
+from studygroups.models import TeamMembership
 
 class ApplicationInline(admin.TabularInline):
     model = Application
 
 class StudyGroupAdmin(admin.ModelAdmin):
-    inlines = [
-        ApplicationInline,
-    ]
+    inlines = [ ApplicationInline ]
 
-class StudyGroupMeetingAdmin(admin.ModelAdmin):
-    pass
+class TeamMembershipInline(admin.TabularInline):
+    model = TeamMembership
 
-class StudyGroupSignupAdmin(admin.ModelAdmin):
-    pass
-
-class CourseAdmin(admin.ModelAdmin):
-    pass
-
-class ActivityAdmin(admin.ModelAdmin):
-    pass
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [ TeamMembershipInline ]
 
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ('name', 'study_group', 'email', 'mobile', 'created_at')
@@ -34,18 +33,13 @@ def reminder_course_title(obj):
 class ReminderAdmin(admin.ModelAdmin):
     list_display = (reminder_course_title, 'email_subject', 'sent_at')
 
-class OrganizerAdmin(admin.ModelAdmin):
-    pass
 
-class FacilitatorAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Course)
+admin.site.register(Activity)
 admin.site.register(StudyGroup, StudyGroupAdmin)
-admin.site.register(StudyGroupMeeting, StudyGroupMeetingAdmin)
+admin.site.register(StudyGroupMeeting)
 admin.site.register(Application, ApplicationAdmin)
 admin.site.register(Reminder, ReminderAdmin)
-admin.site.register(Organizer, OrganizerAdmin)
-admin.site.register(Facilitator, FacilitatorAdmin)
+admin.site.register(Organizer)
+admin.site.register(Facilitator)
+admin.site.register(Team, TeamAdmin)
