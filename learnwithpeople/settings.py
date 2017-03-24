3cae592ad4a2bbd88c99de95711ad37821e7cdfe
Dirk Uys <dirkcuys@gmail.com>
2017-03-24 09:33:44 +0200

    remove old django setting

--- learnwithpeople/settings.py
+++ learnwithpeople/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
