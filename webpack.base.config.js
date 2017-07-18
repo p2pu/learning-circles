09fa144ef27f038c62cd754c20c866ee8d1c0b89
Dirk Uys <dirkcuys@gmail.com>
2017-07-18 13:54:34 +0200

    Add new topnav and footer

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -83,10 +83,14 @@ const styleBuild = {
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [{
-            loader: 'css-loader'
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
           }, {
             loader: 'sass-loader',
             options: {
+              sourceMap: true,
               includePaths: [path.resolve("./static/")]
             }
           }]
