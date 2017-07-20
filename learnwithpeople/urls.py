eaa178d209fb6eebef325c7291f38a1289754baf
Dirk Uys <dirkcuys@gmail.com>
2017-07-20 11:40:50 +0200

    Remove city helper view

--- learnwithpeople/urls.py
+++ learnwithpeople/urls.py
@@ -16,7 +16,6 @@ urlpatterns = i18n_patterns(
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
     url(r'^accounts/login/', auth_views.login, name='login', kwargs={'redirect_authenticated_user': True}),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^ux/', include('uxhelpers.urls')),
     url(r'^jsi18n/$', javascript_catalog, js_info_dict, name='javascript-catalog'),
     url(r'^', include('studygroups.urls'))
 )
--- uxhelpers/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^city_complete/$', views.city_complete, name='uxhelpers_city_complete'),
-]
