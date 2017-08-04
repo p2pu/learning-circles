670b44f265c13606e53314c28f1a5b54d3829d96
Dirk Uys <dirkcuys@gmail.com>
2017-08-04 17:39:52 +0200

    Run wait-for-it with the right path and only wait if running supervisord

--- config/docker-entry.sh
+++ config/docker-entry.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-./wait-for-it.sh postgres:5432
-
 if [ "$1" = 'supervisord' ]; then
+    /wait-for-it.sh postgres:5432
     chown -R celery:celery /var/lib/celery
     /opt/django-venv/bin/python /opt/app/manage.py migrate --noinput
     /opt/django-venv/bin/python /opt/app/manage.py collectstatic --noinput
