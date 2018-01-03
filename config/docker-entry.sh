f1fdaf7bd4c52d5512f091d8ff1ff874701b471b
Dirk Uys <dirkcuys@gmail.com>
2018-01-03 10:21:50 +0200

    Remove supervised from Docker image

--- config/docker-entry.sh
+++ config/docker-entry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'supervisord' ]; then
+if [ "$1" = '/opt/django-venv/bin/gunicorn' ]; then
     chown -R celery:celery /var/lib/celery
     /wait-for-it.sh postgres:5432
     /opt/django-venv/bin/python /opt/app/manage.py migrate --noinput
