d2d0cfa0a3253808f6233192c686e6b0a4c39974
Dirk Uys <dirkcuys@gmail.com>
2018-02-09 13:33:41 +0200

    Fix incorrect imports

--- frontend/components/learning_circle_form/HelpContainer.jsx
+++ frontend/components/learning_circle_form/HelpContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Step1, Step2, Step3, Step4, Step5 } from './helpText'
+import { Step1, Step2, Step3, Step4, Step5 } from './HelpText'
 
 
 export default class CreateLearningCircleForm extends React.Component{
