c654f750922d53585ad9ed670ca9ccf13de20e16
Dirk Uys <dirkcuys@gmail.com>
2018-02-21 11:36:04 +0200

    Add migration and remove `blank=True` from CharField.

--- studygroups/models.py
+++ studygroups/models.py
@@ -179,7 +179,7 @@ class StudyGroup(LifeTimeTrackingModel):
     city = models.CharField(max_length=256)
     latitude = models.DecimalField(max_digits=8, decimal_places=6, null=True, blank=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
-    place_id = models.CharField(max_length=256, null=True, blank=True)
+    place_id = models.CharField(max_length=256, blank=True)
     facilitator = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField()
     meeting_time = models.TimeField()
