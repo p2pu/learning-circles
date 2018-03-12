c4d08e131b3eade6e5638fba701ec40d10961433
Sharon <sharon.peishan.kennedy@gmail.com>
2018-03-12 10:43:18 -0400

    uri encode hidden field data for typeform

--- frontend/facilitator-feedback.jsx
+++ frontend/facilitator-feedback.jsx
@@ -2,10 +2,12 @@ import * as typeformEmbed from '@typeform/embed';
 
 const element = document.getElementById('studygroup-facilitator-feedback');
 const studygroup = element.dataset.studygroup;
-const course = element.dataset.studygroupName;
-const facilitator = element.dataset.facilitator;
-const name = element.dataset.facilitatorName;
+const course = encodeURIComponent(element.dataset.studygroupName);
+const facilitator = encodeURIComponent(element.dataset.facilitator);
+const name = encodeURIComponent(element.dataset.facilitatorName);
 const url = `https://p2pu.typeform.com/to/wPg50i?studygroup=${studygroup}&course=${course}&facilitator=${facilitator}&name=${name}`;
+
+console.log('FACILITATOR URL', url)
 
 const options = {
   onSubmit: () => {
--- frontend/learner-feedback.jsx
+++ frontend/learner-feedback.jsx
@@ -2,8 +2,10 @@ import * as typeformEmbed from '@typeform/embed';
 
 const element = document.getElementById('studygroup-learner-feedback');
 const studygroup = element.dataset.studygroup;
-const course = element.dataset.studygroupName;
+const course = encodeURIComponent(element.dataset.studygroupName);
 const url = `https://p2pu.typeform.com/to/VA1aVz?studygroup=${studygroup}&course=${course}`;
+
+console.log('LEARNER URL', url)
 
 const options = {
   onSubmit: () => {
