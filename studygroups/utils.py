8222f1e5becd7d44daaf0d960d15829d3138aab1
Dirk Uys <dirkcuys@gmail.com>
2018-03-14 13:52:06 +0200

    Add function to turn html emails into text

--- studygroups/utils.py
+++ studygroups/utils.py
@@ -18,3 +18,24 @@ def check_unsubscribe_signature(user, sig):
         ('user', user),
     ]
     return sig == hmac.new(settings.SECRET_KEY, urllib.urlencode(qs), hashlib.sha256).hexdigest()
+
+
+import re
+import bleach
+
+def html_body_to_text(html):
+    # rewrite links
+    link_re = re.compile(r'<a.*?href="(?P<url>.*?)".*?>(?P<text>.*?)</a>')
+    html = link_re.sub(r'\2 ( \1 ) ', html)
+ 
+    # rewrite headings
+    hx_re = re.compile(r'<h\d>(?P<text>.*?)</h\d>')
+    html = hx_re.sub(r'# \1', html)
+
+    # remove all HTML markup
+    txt = bleach.clean(html, tags=[], strip=True)
+
+    # remove leading and tailing whitespace and replace multiple newlines
+    return re.sub(r'\n\s+', '\n\n', txt).strip()
+
+
