56cf15854444a121d9a723f6d4de2b13241d30b5
Sharon <sharon.peishan.kennedy@gmail.com>
2018-08-08 15:32:04 -0400

    minor copy changes

--- frontend/components/learning_circle_form/HelpText.jsx
+++ frontend/components/learning_circle_form/HelpText.jsx
@@ -5,8 +5,8 @@ export const Step1 = () => (
   <div className='help-text'>
     <h4>Course selection tips</h4>
     <div className='content'>
-      <p>The courses here have been added by past facilitators. By default, courses are listed alphabetically but you can also order by the number of times each course has been used. We try to keep this list updated so that only on-demand courses, but you should confirm that a course is available by clicking “see the course” before you go ahead and select it for your learning circle.</p>
-      <p>For more information on how to select a good course, check out <a href="https://community.p2pu.org/t/what-to-look-for-in-a-course/2756" target="_blank" rel="noopener noreferrer">this discussion thread on the community.</a></p>
+      <p>The courses here were added by past facilitators. By default, courses list alphabetically but you can also order by the number of times each course has been used. We try to keep this list limited to on-demand courses, but you should confirm that a course is available by clicking “see the course” before you select it.</p>
+      <p>For more information on how to select a good course, <a href="https://community.p2pu.org/t/what-to-look-for-in-a-course/2756" target="_blank" rel="noopener noreferrer">check out this discussion thread on the community.</a></p>
       <p>If you want to use a course that is not listed on the P2PU courses page, that is fine too! You can use any course from around the web so long as it is free for learners. <a href="https://community.p2pu.org/t/how-do-i-find-a-good-online-course/2757" target="_blank" rel="noopener noreferrer">You can learn more about searching for online courses on the community</a>, as well. Once you find a course you like, you can add that here.</p>
       <a href="/en/course/create/"><button className="p2pu-btn blue">add a course</button></a>
     </div>
