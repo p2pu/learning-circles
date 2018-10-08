ef170ebf9d3d35edd0b1e72e30b665c1a762d4d0
Sharon Kennedy <sharon@nomadiclabs.ca>
2018-10-08 17:12:55 -0400

    try a couple different types of charts

--- studygroups/charts.py
+++ studygroups/charts.py
@@ -16,14 +16,66 @@ custom_style = Style(
   background='transparent',
   plot_background='#ffffff',
   foreground='#515665',
-  foreground_strong='#000000',
+  foreground_strong='#515665',
   foreground_subtle='#6c757d',
   opacity='.6',
   opacity_hover='.9',
   transition='400ms ease-in',
   colors=('#05C6B4', '#B7D500', '#FFBC1A', '#FC7100', '#e83e8c'))
 
+
 class LearnerGoalsChart():
+    def __init__(self, study_group, **kwargs):
+        self.chart = pygal.Dot(stroke=False, show_legend=False, show_y_guides=False, style=custom_style, **kwargs)
+        self.study_group = study_group
+
+    def get_data(self):
+        data = {}
+        learners = self.study_group.application_set
+
+        #TODO this is so dumb lolol
+        data = {
+            "Personal interest": [1,1,1,1,1,1],
+            "Increase employability": [1,1,1,1],
+            "Accompany other education programs": [1,1,1],
+            "Professional development": [1]
+        }
+        return data
+
+    def generate(self):
+        chart_data = self.get_data()
+
+        for key, value in chart_data.items():
+            self.chart.add(key, value)
+
+        return self.chart.render(is_unicode=True)
+
+
+class GoalsMetChart():
+
+    def __init__(self, study_group, **kwargs):
+        self.chart = pygal.Bar(style=custom_style, show_y_guides=False, show_y_labels=False, show_legend=False, **kwargs)
+        self.chart.x_labels = ["1", "2", "3", "4", "5"]
+        self.study_group = study_group
+
+    def get_data(self):
+        data = {}
+        learners = self.study_group.application_set
+        data = {
+            "Rating": [0, 0, 3, 1, 1]
+        }
+        return data
+
+    def generate(self):
+        chart_data = self.get_data()
+
+        for key, value in chart_data.items():
+            self.chart.add(key, value)
+
+        return self.chart.render(is_unicode=True)
+
+
+class SkillsLearnedChart():
 
     def __init__(self, study_group, **kwargs):
         self.chart = pygal.HorizontalBar(style=custom_style, **kwargs)
@@ -32,22 +84,72 @@ class LearnerGoalsChart():
     def get_data(self):
         data = {}
         learners = self.study_group.application_set
-        print(learners)
         data = {
-            "Personal interest": 6,
-            "Increase employability": 4,
-            "Accompany other education programs": 3,
-            "Professional development": 1
+            "Setting goals for myself": 4.29,
+            "Navigating online courses": 3.86,
+            "Working with others": 3.86,
+            "Feeling connected to my community": 3.71,
+            "Speaking in public": 3.71,
+            "Using the internet": 3.71
         }
         return data
 
     def generate(self):
-        # Get chart data
         chart_data = self.get_data()
 
-        # Add data to chart
         for key, value in chart_data.items():
             self.chart.add(key, value)
 
-        # Return the rendered SVG
+        return self.chart.render(is_unicode=True)
+
+
+class NewLearnersChart():
+
+    def __init__(self, study_group, **kwargs):
+        custom_style.value_font_size = 40
+        custom_style.title_font_size = 24
+        self.chart = pygal.SolidGauge(style=custom_style, inner_radius=0.70, show_legend = False, x_title="of participants were taking a learning circle for the first time", **kwargs)
+        self.chart.value_formatter = lambda x: '{:.10g}%'.format(x)
+        self.study_group = study_group
+
+    def get_data(self):
+        data = {}
+        learners = self.study_group.application_set
+
+        data = {
+            'New learners': [{'value': 100, 'max_value': 100}]
+        }
+        return data
+
+    def generate(self):
+        chart_data = self.get_data()
+
+        for key, value in chart_data.items():
+            self.chart.add(key, value)
+
+        return self.chart.render(is_unicode=True)
+
+
+class CompletionRateChart():
+
+    def __init__(self, study_group, **kwargs):
+        self.chart = pygal.SolidGauge(style=custom_style, inner_radius=0.70, show_legend = False, x_title="of participants who responded to the survey completed the learning circle", **kwargs)
+        self.chart.value_formatter = lambda x: '{:.10g}%'.format(x)
+        self.study_group = study_group
+
+    def get_data(self):
+        data = {}
+        learners = self.study_group.application_set
+
+        data = {
+            'Completed': [{'value': 71, 'max_value': 100}]
+        }
+        return data
+
+    def generate(self):
+        chart_data = self.get_data()
+
+        for key, value in chart_data.items():
+            self.chart.add(key, value)
+
         return self.chart.render(is_unicode=True)
--- studygroups/views/reports.py
+++ studygroups/views/reports.py
@@ -8,7 +8,7 @@ from studygroups.models import StudyGroup
 from studygroups.models import Meeting
 from studygroups.models import Course
 from studygroups.models import Application
-from studygroups.charts import LearnerGoalsChart
+from studygroups.charts import LearnerGoalsChart, NewLearnersChart, CompletionRateChart, GoalsMetChart, SkillsLearnedChart
 
 
 
@@ -20,10 +20,18 @@ class StudyGroupFinalReport(TemplateView):
         context = super(StudyGroupFinalReport, self).get_context_data(**kwargs)
         study_group = get_object_or_404(StudyGroup, pk=kwargs.get('study_group_id'))
         learner_goals_chart = LearnerGoalsChart(study_group)
+        new_learners_chart = NewLearnersChart(study_group)
+        completion_rate_chart = CompletionRateChart(study_group)
+        goals_met_chart = GoalsMetChart(study_group)
+        skills_learned_chart = SkillsLearnedChart(study_group)
 
         context = {
             'study_group': study_group,
-            'learner_goals_chart': learner_goals_chart.generate()
+            'learner_goals_chart': learner_goals_chart.generate(),
+            'new_learners_chart': new_learners_chart.generate(),
+            'completion_rate_chart': completion_rate_chart.generate(),
+            'goals_met_chart': goals_met_chart.generate(),
+            'skills_learned_chart': skills_learned_chart.generate(),
         }
 
         return context
