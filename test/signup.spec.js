8e582baf6db5f8569c797f6ebe0b0c9b022959c1
Dirk Uys <dirkcuys@gmail.com>
2018-10-15 18:39:44 +0200

    Basic test to make sure learning circle creation bundle loads

--- test/signup.spec.js
+++ test/signup.spec.js
@@ -1,9 +1,9 @@
-describe('sample test', function () {
+describe('user signup', function () {
   let page;
 
   before (async function () {
     page = await browser.newPage();
-    await page.goto('http://localhost:8000/en/accounts/register/');
+    await page.goto('http://localhost:8000/en/accounts/register/', {timeout: 0, waitUntil: 'domcontentloaded'});
   });
 
   after (async function () {
@@ -11,17 +11,14 @@ describe('sample test', function () {
   })
 
   it('should have the correct page title', async function () {
-    expect(await page.title()).to.eql('Learning circles');
+    let title = await page.title();
+    expect(title).to.eql('Learning circles');
   });
 
-  it('should have a form', async function () {
+
+  it('signup form should have 8 input fields', async function () {
     let form = await page.$('form');
-
-    expect(form).to.eql('Page Title');
-  });
-
-  it('should have 6 input fields', async function () {
-    expect(await page.$$('input')).to.have.lengthOf(6);
+    expect(await form.$$('input')).to.have.lengthOf(8);
   });
 
 });
