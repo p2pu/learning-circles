19c038d53b3c8e3f3668f16f54792d42ddccd102
Dirk Uys <dirkcuys@gmail.com>
2018-10-29 15:43:21 +0200

    Add view to change communication opt-in preferences

--- custom_registration/urls.py
+++ custom_registration/urls.py
@@ -10,6 +10,7 @@ from .views import AjaxLoginView
 from .views import EmailConfirmRequestView
 from .views import EmailConfirmView
 from .views import WhoAmIView
+from .views import AccountSettingsView
 
 
 urlpatterns = [
@@ -21,6 +22,7 @@ urlpatterns = [
     url(r'^email_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', EmailConfirmView.as_view(), name="email_confirm"),
     url(r'^login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
     url(r'^password_reset/$', PasswordResetView.as_view(form_class=CustomPasswordResetForm), name="password_reset"),
+    url(r'^settings/$', AccountSettingsView.as_view(), name="account_settings"),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', PasswordResetConfirmView.as_view(post_reset_login=True, success_url=reverse_lazy('studygroups_login_redirect') ), name='password_reset_confirm'),
     url(r'^', include('django.contrib.auth.urls')),
 ]
