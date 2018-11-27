90a3e22c8daeb26c478758cfaa56a662b89cc031
Sharon Kennedy <sharon@nomadiclabs.ca>
2018-11-27 10:06:19 -0500

    testing registration modal

--- test/learning-circle-bundle.spec.js
+++ test/learning-circle-bundle.spec.js
@@ -1,3 +1,5 @@
+const DOMAIN = 'http://localhost:8000';
+
 describe('learning circle creation', function () {
   let page;
 
@@ -6,13 +8,15 @@ describe('learning circle creation', function () {
     await page.setRequestInterception(true);
     await page.setCacheEnabled(false);
 
+    pageRequests = []
+
     page.on('request', request => {
-      console.log(request.url());
+      console.log(request.url())
       request.continue();
     });
 
     page.on('response', async response => {
-      console.log(response.ok())
+      pageRequests.push({ url: response.url(), response: response })
     });
 
     await page.goto('http://localhost:8000/en/studygroup/create/', {timeout: 0, waitUntil: ['networkidle0', 'load', 'domcontentloaded']});
@@ -27,29 +31,56 @@ describe('learning circle creation', function () {
     expect(title).to.eql('Learning circles');
   });
 
-  it('creation dialog should show location selection', async function () {
-    let tab = await page.$('#react-tabs-1');
-    expect(await tab.$eval('h4', heading => heading.innerText)).to.equal('Step 1: Select a Course *');
-  });
-
   it('should show courses on the courses tab', async function() {
     let tab = await page.$('#react-tabs-1');
     let searchContainer = await page.$('.search-container')
     let resultsCards = await page.$$('.result-item', nodes => node)
+    const coursesApiRequest = pageRequests.find(response => response.url.includes(`${DOMAIN}/api/courses/?order=title`))
 
     expect(await tab.$eval('h4', heading => heading.innerText)).to.equal('Step 1: Select a Course *');
     expect(searchContainer).to.not.be.null
     expect(resultsCards).to.have.lengthOf.above(0)
 
-
-
-    // expect page to make call to api for courses, capture response
-    // expect resultsCards to have the right number of results
+    expect(coursesApiRequest).to.exist
+    expect(coursesApiRequest.response.status()).to.eql(200)
   })
 
-  it('should have a places selector on the location tab')
-  // expect page to make external api call to algolia
-  // expect field to be rendered
+  it('should have a places selector on the location tab', async function() {
+    await Promise.all([
+      page.click("#react-tabs-2"),
+      page.waitForResponse('https://places-dsn.algolia.net/1/places/query/'),
+    ]);
+
+    const placeSelectElement = await page.$(".city-select")
+    const algoliaApiRequest = pageRequests.find(response => response.url.includes(`https://places-dsn.algolia.net/1/places/query/`))
+
+    expect(placeSelectElement).to.exist
+    expect(algoliaApiRequest).to.exist
+    expect(algoliaApiRequest.response.status()).to.eql(200)
+  })
+
+
+  describe('when the user is not logged in', function() {
+    it('should show the user registration modal when the user clicks the publish button', async function() {
+      await page.click("#react-tabs-8")
+      const publishButton = await page.$(".p2pu-btn.publish")
+      await page.click(".p2pu-btn.publish")
+      const registrationModal = await page.$(".registration-modal")
+
+      expect(publishButton).to.exist
+      expect(registrationModal).to.exist
+    })
+
+    it('should show the user registration modal when the user clicks the save button', async function() {
+      await page.click("#react-tabs-8")
+      const saveButton = await page.$(".p2pu-btn.save")
+      await page.click(".p2pu-btn.save")
+      const registrationModal = await page.$(".registration-modal")
+
+      expect(saveButton).to.exist
+      expect(registrationModal).to.exist
+    })
+  })
 
   describe('when the user is logged in', function() {
     before(async function() {
@@ -65,14 +96,43 @@ describe('learning circle creation', function () {
     it('should display errors if missing required fields')
   })
 
-  describe('when the user is not logged in', function() {
-    it('should show the user registration modal when the user clicks the submit button')
-    it('should show the user registration modal when the user clicks the save button')
-  })
-
   describe('registration modal', function() {
-    it('should toggle between registration and log in form')
-    it('should create a user when filled out correctly')
+    before(async function() {
+      await page.click("#react-tabs-8")
+      await page.$(".p2pu-btn.save")
+      await page.click(".p2pu-btn.save")
+      await page.$(".registration-modal")
+    })
+
+    it('should toggle between registration and log in form', async function() {
+      expect(await page.$eval(".registration-modal-content h4", heading => heading.innerText)).to.equal("Create an account")
+      expect(await page.$$(".registration-modal-content input")).to.have.lengthOf(5)
+
+      await page.click(".modal-actions a")
+      expect(await page.$eval(".registration-modal-content h4", heading => heading.innerText)).to.equal("Log in")
+      expect(await page.$$(".registration-modal-content input")).to.have.lengthOf(2)
+
+      await page.click(".modal-actions a")
+      expect(await page.$eval(".registration-modal-content h4", heading => heading.innerText)).to.equal("Create an account")
+      expect(await page.$$(".registration-modal-content input")).to.have.lengthOf(5)
+    })
+
+    it('should create a user when filled out correctly', async function() {
+      page.type("#id_first_name", "Ted")
+      page.type("#id_last_name", "Danson")
+      page.type("#id_email", "teddy@mailinator.net")
+      page.type("#id_password", "teddybear")
+
+      await Promise.all([
+        page.click(".registration-modal-content button[type='submit']"),
+        // page.waitForResponse(`${DOMAIN}/en/accounts/fe/register/`),
+      ]);
+
+      console.log(pageRequests)
+
+      await page.$(".alert-success")
+      expect(await page.$eval(".alert-success .alert-content", alert => alert.innerText)).to.equal("You're logged in! You can now save or publish your learning circle.")
+    })
     it('should show errors when not filled out correctly')
     it('should log in the user when valid credentials are provided')
     it('should not log in the user if invalid credentials are provided')
