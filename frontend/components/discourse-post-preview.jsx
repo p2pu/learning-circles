36aae4e8e6c69f47d7d09ebb700312dee4735f92
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-03-07 14:05:30 -0500

    mobile styling on course page and truncate discourse post preview

--- frontend/components/discourse-post-preview.jsx
+++ frontend/components/discourse-post-preview.jsx
@@ -5,10 +5,13 @@ import moment from 'moment';
 
 const DiscoursePostPreview = ({ post }) => {
   const dateString = new Date(post.created_at).toLocaleDateString("en-US", { year: 'numeric', month: 'short', day: 'numeric' })
+  const regex = new RegExp('^<p>(.*?)<\/p>', 'gm')
+  const truncatedPost = regex.exec(post.cooked)[0]
+
   return (
     <div className="post my-3 mb-4">
       <div className="post-author my-2"><strong>{ post.name }</strong> posted on <strong>{ dateString }</strong>:</div>
-      <div className="post-text" dangerouslySetInnerHTML={{ __html: post.cooked }}></div>
+      <div className="post-text" dangerouslySetInnerHTML={{ __html: truncatedPost }}></div>
     </div>
   )
 }
