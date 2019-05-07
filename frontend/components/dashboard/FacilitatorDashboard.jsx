0875cc02e3442588cc50e70d5cd359136c185cd9
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-05-07 01:17:31 -0400

    add typeform survey links

--- frontend/components/dashboard/FacilitatorDashboard.jsx
+++ frontend/components/dashboard/FacilitatorDashboard.jsx
@@ -190,8 +190,8 @@ export default class FacilitatorDashboard extends React.Component {
               <Card className="bg-dark">
                 <div className="bold text-center text-white">What do you think about your new learning circles dashboard?</div>
                 <div className="text-center mt-3">
-                  <a href="#" className="p2pu-btn light btn-sm">😍 I like it!</a>
-                  <a href="#" className="p2pu-btn light btn-sm">😕 Meh</a>
+                  <a href="https://p2pu.typeform.com/to/WwRKql?first_impression=like" className="p2pu-btn light btn-sm">😍 I like it!</a>
+                  <a href="https://p2pu.typeform.com/to/WwRKql?first_impression=dislike" className="p2pu-btn light btn-sm">😕 Meh</a>
                 </div>
               </Card>
             </div>
