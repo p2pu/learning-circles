ddd2e094dded11043a5d003945e98d09ec77bff3
Dirk Uys <dirkcuys@gmail.com>
2019-07-17 13:42:24 +0200

    Make sure the user is the team organizer before including the invitation URL

--- studygroups/models/team.py
+++ studygroups/models/team.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-from django.utils import timezone
 from django.db import IntegrityError
 from django.conf import settings
 from django.urls import reverse
