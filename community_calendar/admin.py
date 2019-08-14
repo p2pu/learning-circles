747be9bffef2c31a73415b9b9e6c4ecc60bd4db5
Dirk Uys <dirkcuys@gmail.com>
2019-08-14 16:07:28 +0200

    Add community_calendar.models.Event to admin interface

--- community_calendar/admin.py
+++ community_calendar/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Event
+
+class EventAdmin(admin.ModelAdmin):
+
+    list_display = ['datetime', 'title', 'created_by']
+    raw_id_fields = ("created_by",)
+
+
+
+admin.site.register(Event, EventAdmin)
