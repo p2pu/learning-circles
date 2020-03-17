fb2b2e07d2bd4ca926738939ea9afd90c9ce49b9
Dirk Uys <dirkcuys@gmail.com>
2020-03-17 16:56:18 +0200

    remove carousel and survey

--- frontend/components/dashboard/FacilitatorDashboard.jsx
+++ frontend/components/dashboard/FacilitatorDashboard.jsx
@@ -316,48 +316,17 @@ export default class FacilitatorDashboard extends React.Component {
                 </div>
               </Card>
             </div>
-
           </div>
 
           <div className="col-12 col-lg-4">
-
             <div data-aos='fade'>
-
-              <div id="notices" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                  <li data-target="#notices" data-slide-to="0" class="active"></li>
-                  <li data-target="#notices" data-slide-to="1"></li>
-                </ol>
-                <div className="carousel-inner">
-                  <div className="carousel-item active">
-                    <Card className="bg-warning feedback-survey d-block w-100">
-                      <div className="bold text-center text-white">In light of the worldwide Coronavirus, meeting in person is probably not an option for your learning circle right now. How do we learn together when we cannot meet in person?
-                      </div>
-                      <div className="text-center mt-3">
-                        <a href="https://community.p2pu.org/t/how-do-we-learn-together-when-we-cant-meet-together-a-place-to-talk-about-how-were-responding-to-covid-19/4366" className="p2pu-btn light btn-sm">Learn more and share your experience</a>
-                      </div>
-                    </Card>
-                  </div>
-                  <div className="carousel-item">
-                    <Card className="bg-dark feedback-survey d-block w-100">
-                      <div className="bold text-center text-white">What do you think about your new learning circles dashboard?</div>
-                      <div className="text-center mt-3">
-                        <a href="https://p2pu.typeform.com/to/WwRKql?first_impression=like" className="p2pu-btn light btn-sm">😍 I like it!</a>
-                        <a href="https://p2pu.typeform.com/to/WwRKql?first_impression=dislike" className="p2pu-btn light btn-sm">😕 Meh</a>
-                      </div>
-                    </Card>
-                  </div>
+              <Card className="bg-warning feedback-survey d-block w-100">
+                <div className="bold text-center text-white">In light of the worldwide coronavirus pandemic, meeting in person is probably not an option for your learning circle right now. How do we learn together when we cannot meet in person?
                 </div>
-                <a class="carousel-control-prev" href="#notices" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#notices" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-              </div>
-              
+                <div className="text-center mt-3">
+                  <a href="https://community.p2pu.org/t/how-do-we-learn-together-when-we-cant-meet-together-a-place-to-talk-about-how-were-responding-to-covid-19/4366" className="p2pu-btn light btn-sm" target="_blank">Learn more and share your experience</a>
+                </div>
+              </Card>
             </div>
 
             <div data-aos='fade'>
