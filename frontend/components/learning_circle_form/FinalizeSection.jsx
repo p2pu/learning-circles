c4e06f21eb59b7a22f96415f34cc0de90ae525bb
Dirk Uys <dirkcuys@gmail.com>
2020-05-14 20:03:16 +0200

    Remove unused code

--- frontend/components/learning_circle_form/FinalizeSection.jsx
+++ frontend/components/learning_circle_form/FinalizeSection.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
 import TextareaWithLabel from 'p2pu-input-fields/dist/TextareaWithLabel'
 
-const FinalizeSection = (props) => {
-  const handleImageUpload = (pictures) => {
-    props.updateFormData({ image: pictures })
-  }
-
-  return (
-    <div>
-      <TextareaWithLabel
-        label={'What do you hope to achieve by facilitating this learning circle?'}
-        value={props.learningCircle.facilitator_goal || ''}
-        handleChange={props.updateFormData}
-        name={'facilitator_goal'}
-        id={'id_facilitator_goal'}
-        errorMessage={props.errors.facilitator_goal}
-      />
-      <TextareaWithLabel
-        label={'Is there anything that we can help you with as you get started?'}
-        value={props.learningCircle.facilitator_concerns || ''}
-        handleChange={props.updateFormData}
-        name={'facilitator_concerns'}
-        id={'id_facilitator_concerns'}
-        errorMessage={props.errors.facilitator_concerns}
-      />
-    </div>
-  )
-}
+const FinalizeSection = (props) => (
+  <div>
+    <TextareaWithLabel
+      label={'What do you hope to achieve by facilitating this learning circle?'}
+      value={props.learningCircle.facilitator_goal || ''}
+      handleChange={props.updateFormData}
+      name={'facilitator_goal'}
+      id={'id_facilitator_goal'}
+      errorMessage={props.errors.facilitator_goal}
+    />
+    <TextareaWithLabel
+      label={'Is there anything that we can help you with as you get started?'}
+      value={props.learningCircle.facilitator_concerns || ''}
+      handleChange={props.updateFormData}
+      name={'facilitator_concerns'}
+      id={'id_facilitator_concerns'}
+      errorMessage={props.errors.facilitator_concerns}
+    />
+  </div>
+);
 
 export default FinalizeSection;
