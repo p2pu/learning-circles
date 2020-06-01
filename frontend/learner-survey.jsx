04ae6b14a436a14a1706309ad8c20965d339a17c
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-06-01 12:58:25 -0400

    add studygroup name to typeoform survey templates

--- frontend/learner-survey.jsx
+++ frontend/learner-survey.jsx
@@ -3,13 +3,14 @@ import * as typeformEmbed from '@typeform/embed';
 const element = document.getElementById('studygroup-learner-feedback');
 const surveyId = element.dataset.surveyId;
 const studygroupUuid = element.dataset.studygroupUuid;
+const studygroupName = element.dataset.studygroupName;
 const course = encodeURIComponent(element.dataset.course);
 const contact = encodeURIComponent(element.dataset.contact);
 const goalRating = encodeURIComponent(element.dataset.goalRating);
 const learnerUuid = element.dataset.learnerUuid
 const facilitator = encodeURIComponent(element.dataset.facilitatorName)
 const goalmet = element.dataset.goalMet;
-const url = `https://p2pu.typeform.com/to/${surveyId}?studygroup_uuid=${studygroupUuid}&course=${course}&goal_rating=${goalRating}&learner_uuid=${learnerUuid}&facilitator=${facilitator}`;
+const url = `https://p2pu.typeform.com/to/${surveyId}?studygroup_uuid=${studygroupUuid}&studygroup_name=${studygroupName}&course=${course}&goal_rating=${goalRating}&learner_uuid=${learnerUuid}&facilitator=${facilitator}`;
 
 
 const options = {
