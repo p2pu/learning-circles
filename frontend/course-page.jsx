12fb72ca136e46e622ab7bbccc334f7a535d216f
Dirk Uys <dirkcuys@gmail.com>
2020-06-23 19:54:22 +0200

    Add command + logic to remove discourse links for courses where no discussion happened and update the way it is displayed on the course page

--- frontend/course-page.jsx
+++ frontend/course-page.jsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom'
 import DiscourseTopicPreview from './components/discourse-topic-preview'
 import SimilarCoursesPreview from './components/similar-courses-preview'
 
+const discussion = document.getElementById('course-discussion-preview');
+if (discussion !== null) {
+  const topicUrl = discussion.dataset.topicUrl;
+  ReactDOM.render(<DiscourseTopicPreview topicUrl={topicUrl} />, discussion);
+}
 
-const discussion = document.getElementById('course-discussion-preview')
-const similarCourses = document.getElementById('similar-courses')
-const courses = similarCourses.dataset.courses
-const topicUrl = discussion.dataset.topicUrl;
-const discourseText = discussion.dataset.discourseText
-
-console.log(discourseText)
-
-ReactDOM.render(<DiscourseTopicPreview topicUrl={topicUrl} discourseText={discourseText} />, discussion)
-ReactDOM.render(<SimilarCoursesPreview courses={courses} />, similarCourses)
+const similarCourses = document.getElementById('similar-courses');
+const courses = similarCourses.dataset.courses;
+ReactDOM.render(<SimilarCoursesPreview courses={courses} />, similarCourses);
