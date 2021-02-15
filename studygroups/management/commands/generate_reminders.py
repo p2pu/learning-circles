afe90ba2a9720ffd80780e7696353510501362c7
Dirk Uys <dirkcuys@gmail.com>
2021-02-15 10:27:16 +0200

    Update task to generate reminders for all future meetings

--- studygroups/management/commands/generate_reminders.py
+++ studygroups/management/commands/generate_reminders.py
@@ -1,9 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
 
-from studygroups.tasks import gen_reminders
+from studygroups.models import Meeting
+from studygroups.models.learningcircle import generate_meeting_reminder
 
 class Command(BaseCommand):
-    help = 'Generate reminders for all study groups happening in 3 days from now'
+    help = 'Transitional command to generate reminders for all meetings in the future.'
 
     def handle(self, *args, **options):
-        gen_reminders()
+        today = timezone.now().date()
+        meetings = Meeting.objects.active().filter(study_group__deleted_at__isnull=True).filter(meeting_date__gte=today)
+        for meeting in meetings:
+            print(f'Generating meeting reminder for meeting happening {meeting.meeting_date}')
+            generate_meeting_reminder(meeting)
