843887ff28db6d98c92e6c02f454abb842f4d988
Dirk Uys <dirkcuys@gmail.com>
2022-01-25 11:28:53 +0200

    Remove facilitator list and invitation component from old organizer dash. Closes #980

--- frontend/components/organizer-dash.jsx
+++ frontend/components/organizer-dash.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import TabSelector from './tab-selector'
 import WeeklyMeetingsList from './weekly-meetings-list'
 import ActiveLearningCirclesList from './active-learning-circles-list'
-import FacilitatorList from './facilitator-list'
-import InvitationList from './invitation-list'
 
 require("./stylesheets/organizer-dash.scss");
 
@@ -12,16 +10,12 @@ export default class OrganizerDash extends React.Component{
         return (
             <div className="organizer-dash">
                 <h1>{gettext("Organizer Dashboard")}</h1>
-                <TabSelector header={[gettext('Upcoming Meetings'), gettext('Active Learning Circles'), gettext('Facilitators'), gettext('Invitations')]} >
+                <TabSelector header={[gettext('Upcoming Meetings'), gettext('Active Learning Circles')]} >
                     <WeeklyMeetingsList
                         meetings={this.props.meetings} 
                         learningCircles={this.props.activeLearningCircles} />
                     <ActiveLearningCirclesList 
                         learningCircles={this.props.activeLearningCircles} />
-                    <FacilitatorList facilitators={this.props.facilitators} />
-                    <InvitationList
-                        teamInviteUrl={this.props.teamInviteUrl}
-                        invitations={this.props.invitations} />
                 </TabSelector>
             </div>
         );
