22c3ec44f208f4fb61a6d5dc4dc0908c1fb49ac3
Dirk Uys <dirkcuys@gmail.com>
2022-10-12 09:16:25 +0200

    Add comment to explain purspose of react view for course page

--- frontend/course-page.jsx
+++ frontend/course-page.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import CourseLearningCircles from './components/course-learning-circles'
 
+/*
+ * Add react component to page to display learning circles associated with the course
+ */
+
 const dataEl = document.getElementById('course-learning-circles-data');
 const learningCircles = JSON.parse(dataEl.textContent);
 const reactRoot = document.getElementById('course-learning-circles');
