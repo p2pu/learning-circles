d57881ae2295a32fa1e0413df6762ecce05a6a74
Dirk Uys <dirkcuys@gmail.com>
2022-12-15 12:50:51 +0200

    Add boilerplate for meeting RSVPs and participant surveys

--- frontend/components/participant-dash.jsx
+++ frontend/components/participant-dash.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import { date, day, time } from 'p2pu-components/dist/utils/i18n';
 
+const Rsvp = props => {
+  // TODO post meeting_id + yes/no
+  const onRsvp = e => {
+    e.preventDefault();
+    fetch('/rsvp', {
+      method: 'POST',
+      data: {}
+    });
+  }
+
+  return (
+    <div>
+      { (props.rsvp) 
+        && <p>You RSVPed {props.rsvp?'yes':'no'} for this meeting.</p>
+      }
+      { (!props.rsvp && props.meetingId) && 
+        <p>Can you make this meeting? <button className="btn p2pu-btn orange">yes</button> / <button className="btn p2pu-btn orange">no</button></p> 
+      }
+    </div>
+  );
+};
+
 const MeetingCard = props => {
   const {meeting_date, meeting_time} = props;
+  const {rsvp} = props;
   const formattedDate = date(meeting_date, 'en-us');
   const formattedTime = time(meeting_time);
   const done = new Date() > new Date(props.meeting_datetime);
@@ -10,11 +33,16 @@ const MeetingCard = props => {
     <div className={`item meeting ${done?'done':'todo'}`} id={`meeting-${props.id}`}>
       <div className="icon"></div>
       <div className="card">
-        <button className={"card-collapse-toggle" + (done?' collapsed':'')} data-bs-toggle="collapse" data-bs-target={`#collapse-meeting-${props.id}`} type="button" aria-expanded={!done} aria-controls={`collapse-meeting-${props.id}`}><i className="fa fa-chevron-down"></i></button>
+        { (rsvp || !done) &&
+          <button className={"card-collapse-toggle" + (done?' collapsed':'')} data-bs-toggle="collapse" data-bs-target={`#collapse-meeting-${props.id}`} type="button" aria-expanded={!done} aria-controls={`collapse-meeting-${props.id}`}><i className="fa fa-chevron-down"></i></button>
+        }
         <div className="card-title">Meeting #{props.meeting_number}: {formattedDate} {formattedTime}</div>
-
-        <div class="collapse" id={`collapse-meeting-${props.id}`}>
-        </div>
+        
+        { (rsvp || !done) &&
+          <div class={"collapse" + (!done?'.show':'')} id={`collapse-meeting-${props.id}`}>
+            <Rsvp rsvp={rsvp} meetingId={props.meeting_id} />
+          </div>
+        }
       </div>
     </div>
   );
@@ -30,6 +58,21 @@ const MessageCard = props => {
         <div>Sent: {date(props.sent_at)}</div>
         <div class="collapse" id={`collapse-meeting-${props.id}`}>
           <div class="email-preview p-2" dangerouslySetInnerHTML={{__html: props.body}}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const SurveyCard = props => {
+  return (
+    <div className="item survey todo" id="survey">
+      <div className="icon"></div>
+      <div className="card">
+        <button className="card-collapse-toggle" data-bs-toggle="collapse" data-bs-target="#collapse-survey" type="button" aria-expanded="true" aria-controls="collapse-survey"><i className="fa fa-chevron-down"></i></button>
+        <div className="card-title">Reflect</div>
+        <div class="collapse.show" id="collapse-survey">
+          <p>Complete survey</p>
         </div>
       </div>
     </div>
@@ -73,6 +116,7 @@ const ParticipantDash = props => {
           return <item.component {...item.data} key={i} id={i} />
         })
       }
+      <SurveyCard />
     </div>
   );
 }
