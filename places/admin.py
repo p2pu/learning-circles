886a2c5f59ccfdeab071b09ad2a22fd32b1fc292
Dirk Uys <dirkcuys@gmail.com>
2023-01-05 15:25:20 +0200

    Add search fields for cities in django admin

--- places/admin.py
+++ places/admin.py
@@ -5,6 +5,7 @@ from .models import City
 
 class CityAdmin(admin.ModelAdmin):
     list_display = ['city', 'region', 'country', 'country_en', 'latitude', 'longitude']
+    search_fields = ['city', 'region', 'country', 'country_en']
 
 
 admin.site.register(City, CityAdmin)
