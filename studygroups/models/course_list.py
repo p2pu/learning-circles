9e6994a5480b89f2dd74aaf0db1c39a17a86860e
Dirk Uys <dirkcuys@gmail.com>
2024-02-06 15:54:42 +0200

    Add __str__ to have sensible labels in django admin

--- studygroups/models/course_list.py
+++ studygroups/models/course_list.py
@@ -4,3 +4,8 @@ from django.contrib.auth.models import User
 class CourseList(models.Model):
     team = models.ForeignKey('studygroups.Team', on_delete=models.CASCADE)
     courses = models.ManyToManyField('studygroups.Course')
+
+    def __str__(self):
+        return f'{self.team.name} course list ({self.courses.count()})'
+
+
