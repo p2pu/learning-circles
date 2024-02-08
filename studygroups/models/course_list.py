a2cd2d1a9a16b6351587e4b6c4cde657b5378c14
Dirk Uys <dirkcuys@gmail.com>
2024-02-08 15:29:40 +0200

    Allow empty course lists

--- studygroups/models/course_list.py
+++ studygroups/models/course_list.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class CourseList(models.Model):
     team = models.ForeignKey('studygroups.Team', on_delete=models.CASCADE)
-    courses = models.ManyToManyField('studygroups.Course')
+    courses = models.ManyToManyField('studygroups.Course', blank=True)
 
     def __str__(self):
         return f'{self.team.name} course list ({self.courses.count()})'
